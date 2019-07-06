f5a9c5f04652e2b695c58f927304aef6b7a060c1
Tanmay Datta <tanmay.datta86@gmail.com>
2019-07-06 23:06:40 +1000

    setup fixed

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/fattmerchantorg/fattmerchant_python_client",
     packages=setuptools.find_packages(),
+    install_requires =["ddt", "requests"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
