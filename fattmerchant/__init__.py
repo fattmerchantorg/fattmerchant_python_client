c6366e3b822fdb25b1ee3abe56c110e783c22cb0
Austin Burns <austin.c.burns@gmail.com>
2019-07-24 14:11:46 -0400

    Modify the logging file path to be found outside of the module

--- fattmerchant/__init__.py
+++ fattmerchant/__init__.py
@@ -1,8 +1,9 @@
-#test tanmay
-name = "fattmerchant"
-import logging.config 
+import logging.config
+
 from os import path
-log_file_path = path.join(path.dirname(path.abspath(__file__)),
- 'logging.config')
-logging.config.fileConfig(log_file_path,
+
+name = "fattmerchant"
+
+file_dir = path.split(path.realpath(__file__))[0]
+logging.config.fileConfig(path.join(file_dir, 'logging.config'),
                           disable_existing_loggers=False)
