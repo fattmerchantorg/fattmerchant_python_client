964b20aaeaaf09995a5f53c27cd95f8a9ee9046b
Johann <johann.nieto@fattmerchant.com>
2019-10-31 09:53:39 -0400

    release version 1.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fattmerchant",
-    version="1.1.7",
+    version="1.1.8",
     author="Fattmerchant INC",
     author_email="support@fattmerchant.com",
     description="Python Client for Fattmerchant's Omni API",
